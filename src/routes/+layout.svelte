0950230a1e43d685487292bdf3d1992fb0343fa0
Harry Xu <92757553+HarryXu497@users.noreply.github.com>
2024-06-05 17:45:37 -0400

    fixed some deployment issues

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import "../styles/reset.scss";
 	import "../styles/globals.scss";
+
+	import type { Config } from '@sveltejs/adapter-vercel';
+
+	export const config: Config = {
+		runtime: 'nodejs18.x',
+	};
 </script>
 
 <main>
